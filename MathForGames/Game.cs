7a9ad821ea01a1611276836c08c191547e33d523
Ben Brownlie <benjaminbrownlie@gmail.com>
2020-10-12 17:05:02 -0500

    Created actor array and console movement
    Created an array that adds and removes actors. Started working on functions that read keypresses and move a character across the screen accordingly

--- MathForGames/Game.cs
+++ MathForGames/Game.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace MathForGames
 {
     class Game
     {
         private static bool _gameOver = false;
-
+        private Scene _scene;
         //Static function used to set game over without an instance of game.
         public static void SetGameOver(bool value)
         {
@@ -16,23 +17,40 @@ namespace MathForGames
         }
 
 
+        //Return whether or not the specified ConsoleKey is pressed
+        public static bool CheckKey(ConsoleKey key)
+        {
+            if (Console.KeyAvailable)
+            {
+                if (Console.ReadKey(true).Key == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         //Called when the game begins. Use this for initialization.
         public void Start()
         {
-
+            _scene = new Scene();
+            Actor actor = new Actor();
+            _scene.AddActor(actor);
         }
 
 
         //Called every frame.
         public void Update()
         {
-
+            _scene.Update();
         }
 
         //Used to display objects and other info on the screen.
         public void Draw()
         {
-
+            Console.Clear();
+            _scene.Draw();
         }
 
 
@@ -52,6 +70,8 @@ namespace MathForGames
             {
                 Update();
                 Draw();
+                while (Console.KeyAvailable) Console.ReadKey(true);
+                Thread.Sleep(250);
             }
 
             End();
